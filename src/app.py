c85e9aded154d29c0de4e44e9d1e6daae5e1c05f
ianlucasch <ianlucaschaves@gmail.com>
2024-07-16 10:13:46 -0300

    Modelos de dados

--- src/app.py
+++ src/app.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import url_for
-from flask import request
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
--- src/app2.py
+++ src/app2.py
@@ -1,14 +1,32 @@
 import os
 import click
-from flask import Flask
-from flask import current_app
+from datetime import datetime
+from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
+import sqlalchemy as sa
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Base(DeclarativeBase):
     pass
 
 db = SQLAlchemy(model_class=Base)
+
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, username={self.username!r})"
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey("user.id"))
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id!r}, title={self.title!r}, author_id{self.author_id!r})"
 
 @click.command("init-db")
 def init_db_command():
