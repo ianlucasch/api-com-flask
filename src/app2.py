bac846f82674988edad5f905546486157ec2759d
ianlucasch <ianlucaschaves@gmail.com>
2024-07-16 09:32:19 -0300

    Conexão com banco de dados com Flask-SQLAlchemy

--- src/app2.py
+++ src/app2.py
@@ -1,10 +1,27 @@
+import os
+import click
 from flask import Flask
+from flask import current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+
+@click.command("init-db")
+def init_db_command():
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo("Initialized the database.")
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY ="dev",
-        DATABASE="db.sqlite"
+        SQLALCHEMY_DATABASE_URI="sqlite:///my_db.sqlite"
     )
 
     if test_config is None:
@@ -12,7 +29,15 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    from . import db
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # register cli commands
+    app.cli.add_command(init_db_command)
+
+    # initialize extensions
     db.init_app(app)
 
     return app
